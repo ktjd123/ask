e84cab43c4679ea9e45614c120c35ce9951c3313
ktjd1262 <k9326239@gmail.com>
2018-01-04 21:51:12 +0900

    login description change

--- frontend/src/components/Template/LoginTemplate/LoginTemplate.js
+++ frontend/src/components/Template/LoginTemplate/LoginTemplate.js
@@ -9,6 +9,9 @@ const LoginTemplate = ({Login, onChange, onLogin, id, pw}) => {
             <section className='loginT'>
                 <Login onChange={onChange} onLogin={onLogin} id={id} pw={pw}/>
             </section>
+            <section className='des'>
+                로그인을 하고 친구들에게 익명 질문, 응원을 받아보세요!
+            </section>
         </div>
     );
 };
